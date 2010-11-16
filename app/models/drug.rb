16140b09a914a1fe621f4d33f4d2748002107ba2
mdms-scott <mdms.scott@gmail.com>
2010-11-16 03:40:12 -0500

    added more drug types

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,6 +1,6 @@
 class Drug < ActiveRecord::Base
   
-  TYPES = ['Antibiotic', 'Statin', 'Anti-Fungal', 'Anti-Histamine', 'Anti-Depressant', 'Sedative', 'Anti-Psychotic']
+  TYPES = ['Antibiotic', 'Statin', 'Anti-Fungal', 'Anti-Histamine', 'Anti-Depressant', 'Sedative', 'Anti-Psychotic', 'Steroid', 'Anti_Viral', 'Analgesic', 'Laxitive', 'Diuretic', 'Antacid', 'Anti-Coagulant', 'Anti-Convulsant', 'Anti-Diarrheal', 'Hormone', 'Bronchodilator', 'Decongestant', 'Heparin', 'Leprostatic', 'Nitrate', 'Muscle Relaxant', 'Vasodilator', 'Vasoconstictor']
 
   validates_presence_of :name, :kind, :code, :purpose
   validates_uniqueness_of :interactions
