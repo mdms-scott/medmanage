5a28b680bffcf8f41a7f2ca7bb0c5646b44be3c1
mdms-scott <mdms.scott@gmail.com>
2010-11-16 14:02:30 -0500

    more robust readme

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -3,4 +3,5 @@ class Medication < ActiveRecord::Base
   belongs_to :drug
   
   validates_presence_of :patient_id, :drug_id, :prescribing, :prescription, :dose, :quantity, :filled_on, :refills
+  validates_uniqueness_of :prescription
 end
